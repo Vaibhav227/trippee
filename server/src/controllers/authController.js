826c682c413d82a39042977a3bce251337421172
Vaibhav <sharmavaibhav227@gmail.com>
2023-06-23 20:54:43 +0530

    new practices

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -5,10 +5,10 @@ import { validateSignup } from "../validator.js"
 
 export const signup = async (req, res) => {
 
-    const x = validateSignup(req.body);
-    if (x.error) {
-        console.log(x.error)
-        return res.send(x.error.details);
+    const signUpError = validateSignup(req.body);
+    if (signUpError.error) {
+        console.log(signUpError.error)
+        return res.status(403).send(signUpError.error.details);
     }
     res.send("Successfully signed up")
 
@@ -28,57 +28,12 @@ export const signup = async (req, res) => {
     } catch (error) {
         res.json({ error: error.message })
     }
-    // bcrypt.hash(user.password, 12)
-    //     .then(hashed_password => {
-    //         return database("users")
-    //             .insert({
-    //                 username: user.username,
-    //                 password: hashed_password
-    //             })
-    //             .returning("*")
-    //             .then(users => {
-    //                 const user = users[0]
-    //                 res.json({ user })
-    //             }).catch(error => {
-    //                 res.json({ error: error.message })
-    //             })
-    //     })
+
 }
 
 export const login = async (req, res) => {
-    // const { email, password } = req.body
-    // if (!email || !password) {
-    //     return next(new AppError('Please provide email and password!', 400));
-    // }
-    // let user 
-    // findUser(email)
-    // .then(foundUser => {
-    //   user = foundUser
-    //   return checkPassword(password, foundUser)
-    // })
-    // .then((res) => createSendToken())
-    // .catch((err) => console.error(err))
+
     let user = req.body
-    // database("users")
-    //     .where({ username: user.username })
-    //     .first()
-    //     .then(retrievedUser => {
-    //         if (!retrievedUser) throw new Error("user not found!")
-    //         return Promise.all([
-    //             bcrypt.compare(user.password, retrievedUser.password),
-    //             Promise.resolve(retrievedUser)
-    //         ]).then(results => {
-    //             const areSamePasswords = results[0]
-    //             if (!areSamePasswords) throw new Error("wrong Password!")
-    //             const user = results[1]
-    //             const payload = { username: user.username }
-    //             const secret = "SECRET"
-    //             jwt.sign(payload, secret, (error, token) => {
-    //                 if (error) throw new Error("Sign in error!")
-    //                 res.json({ token, user })
-    //             })
-    //         })
-    //     })
     let retrievedUser = await database("users")
         .where({ username: user.username })
         .first()
